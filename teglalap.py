cbcc7a20feeece516b3cc2bc4a07bc882174502a
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2022-02-04 18:47:43 +0100

    Add files via upload

---
+++ teglalap.py
@@ -0,0 +1,11 @@
+print ("A téglalap kerülete:[0] egység." .format (K))
+print ("A téglalap területe:[0] négyzetegység." .format (T))
+K = def kerulet(a,b):
+    return(2*(a+b))
+T = def terulet(a,b):
+    return(a*b)
+
+a = float(input("A oldal:"))
+b = float(input("B oldal:"))
+K = kerulet(a,b)
+T = terulet(a,b)
